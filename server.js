f29ecb82b4c35bafb767089b72d74764dbe168a4
nithesh1212 <nithesh.machiraju@kore.com>
2018-01-18 10:47:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-app.use(express.static(__dirname + '/UI'));
+app.use(express.static(__dirname + '/'));
 app.set('port', (process.env.PORT || 5000)); 
 app.get('/', function (req, res) {
 	res.header("Content-Type", "text/html");
 	console.log('*****************************');
-	fs.readFile( __dirname + "/" + "entellioChat.html", 'utf8', function (err, data) {
+	fs.readFile( __dirname + "/" + "UI/index.html", 'utf8', function (err, data) {
 	console.log('*****************************')
 	console.log(req.url);
     res.end( data );
